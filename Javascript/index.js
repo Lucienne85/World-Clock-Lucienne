86946635c40e549935a9566e224c37fa6c521d77
Lucienne85 <66264188+Lucienne85@users.noreply.github.com>
2022-10-27 14:18:43 +0200

    Added the second location

--- Javascript/index.js
+++ Javascript/index.js
@@ -2,12 +2,22 @@ let portlandBlock = document.querySelector("#portland");
 let portlandDate = portlandBlock.querySelector(".date");
 let portlandTime = document.querySelector("#portland-time");
 
+let amsterdamBlock = document.querySelector("#amsterdam");
+let amsterdamDate = amsterdamBlock.querySelector(".date");
+let amsterdamTime = document.querySelector("#amsterdam-time");
+
+
 function showCurrentTime() {
 let portlandCurrentTime = moment().tz("America/Vancouver");
+let amsterdamCurrentTime = moment().tz("Europe/Amsterdam");
 
 portlandDate.innerHTML = portlandCurrentTime.format("MMMM Do YYYY");
 portlandTime.innerHTML = portlandCurrentTime.format("h:mm:ss [<small>]A[</small>]");
+
+amsterdamDate.innerHTML = amsterdamCurrentTime.format("MMMM Do YYYY");
+amsterdamTime.innerHTML = amsterdamCurrentTime.format("h:mm:ss [<small>]A[</small>]");
 }
+
 
 showCurrentTime();
 setInterval(showCurrentTime, 1000);

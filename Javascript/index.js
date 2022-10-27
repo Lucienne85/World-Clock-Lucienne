8b571562ee1b87860eb8e0d6330477409f1b1302
Lucienne85 <66264188+Lucienne85@users.noreply.github.com>
2022-10-27 14:10:16 +0200

    Set the 1 second interval for the first location

--- Javascript/index.js
+++ Javascript/index.js
@@ -2,7 +2,13 @@ let portlandBlock = document.querySelector("#portland");
 let portlandDate = portlandBlock.querySelector(".date");
 let portlandTime = document.querySelector("#portland-time");
 
+function showCurrentTime() {
 let portlandCurrentTime = moment().tz("America/Vancouver");
 
 portlandDate.innerHTML = portlandCurrentTime.format("MMMM Do YYYY");
 portlandTime.innerHTML = portlandCurrentTime.format("h:mm:ss [<small>]A[</small>]");
+}
+
+showCurrentTime();
+setInterval(showCurrentTime, 1000);
+

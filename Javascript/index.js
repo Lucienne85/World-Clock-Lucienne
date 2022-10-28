8dc319f2c3cba62a1339c56f1b239cd6e85c95c3
Lucienne85 <66264188+Lucienne85@users.noreply.github.com>
2022-10-28 21:49:18 +0200

    Made the drop down menu work

--- Javascript/index.js
+++ Javascript/index.js
@@ -18,7 +18,31 @@ amsterdamDate.innerHTML = amsterdamCurrentTime.format("MMMM Do YYYY");
 amsterdamTime.innerHTML = amsterdamCurrentTime.format("h:mm:ss [<small>]A[</small>]");
 }
 
+function updateCityTime(event) {
+let chosenCity = event.target.value;
+if (chosenCity.length > 0){
+
+let chosenCityName = chosenCity.replace("_", " ").split("/")[1];
+let chosenCityTime = moment().tz(chosenCity);
+let cityTimeElement = document.querySelector("#city-time-element");
+
+cityTimeElement.innerHTML = `
+<div class="city">
+        <div class="defaultCity">
+            <div class="cityNameDate">
+                <h2 class="cityName">${chosenCityName}</h2>
+                <div class="date">${chosenCityTime.format("MMMM Do YYYY")}</div>
+            </div>
+        </div>
+        <div class="time">${chosenCityTime.format("h:mm:ss")} <small>${chosenCityTime.format("A")}</small></div>
+    </div>
+`
+}
+}
+
 
 showCurrentTime();
 setInterval(showCurrentTime, 1000);
 
+let citySelector = document.querySelector("#citySelect");
+citySelector.addEventListener('change', updateCityTime);
